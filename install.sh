f787a6113edc1f64cc9433203b1c4060b3dafc65
chenwei850825 <chenwei850825@gmail.com>
2018-08-16 14:54:16 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-node -e "const peers = Object.entries(require('./package.json').peerDependencies || {}).map(d => d.join('@')).join(' '); if (peers.length) process.stdout.write('yarn global add -P --no-lockfile ' + String(peers));" | sh
